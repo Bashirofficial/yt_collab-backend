c564f46ca6adbc9b8808245cfa38172da3f81f11
BASHIR AHMED MONDAL <bashirmondal03@gmail.com>
2025-08-26 22:29:52 +0530

    update: project schema is being updated

---
+++ prisma/migrations/20250826165534_add_project_code_v5/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "projects_projectDisplayId_idx" ON "public"."projects"("projectDisplayId");
+
+-- CreateIndex
+CREATE INDEX "projects_youtuberId_idx" ON "public"."projects"("youtuberId");
