76f688e5eae7ba0cdc82d993152b5207489adbf9
BASHIR AHMED MONDAL <bashirmondal03@gmail.com>
2025-09-05 21:08:17 +0530

    new: get file url  route is being added

--- src/routes/file.route.ts
+++ src/routes/file.route.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { uploadFile, getProjectFiles, getFileById } from "../controllers/file.controller";
+import {
+  uploadFile,
+  getProjectFiles,
+  getFileById,
+  generateFileSignedUrl,
+} from "../controllers/file.controller";
 import { authenticate } from "../middlewares/auth.middleware";
 
 const router = Router({ mergeParams: true });
@@ -7,5 +12,6 @@ const router = Router({ mergeParams: true });
 router.route("/upload-files").post(authenticate, uploadFile);
 router.route("/get-project").get(authenticate, getProjectFiles);
 router.route("/get-file").get(authenticate, getFileById);
+router.route("/get-file-url").get(authenticate, generateFileSignedUrl);
 
 export default router;
