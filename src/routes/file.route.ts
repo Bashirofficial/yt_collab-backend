3e5d8cdf43d1c4befaa4e14fb7f231880abfcd77
BASHIR AHMED MONDAL <bashirmondal03@gmail.com>
2025-09-07 18:03:38 +0530

    new: delete file route is being added

--- src/routes/file.route.ts
+++ src/routes/file.route.ts
@@ -5,6 +5,7 @@ import {
   getFileById,
   generateFileSignedUrl,
   updateFileStatus,
+  deleteFile,
 } from "../controllers/file.controller";
 import { authenticate } from "../middlewares/auth.middleware";
 
@@ -15,5 +16,6 @@ router.route("/get-project").get(authenticate, getProjectFiles);
 router.route("/get-file").get(authenticate, getFileById);
 router.route("/get-file-url").get(authenticate, generateFileSignedUrl);
 router.route("/update-file-status").post(authenticate, updateFileStatus);
+router.route("/delete-file").post(authenticate, deleteFile);
 
 export default router;
