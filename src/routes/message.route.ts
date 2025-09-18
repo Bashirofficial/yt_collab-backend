922ad5e695db1c0c150b8b1043dcc063ea07ff53
BASHIR AHMED MONDAL <bashirmondal03@gmail.com>
2025-09-18 21:35:57 +0530

    add: pending routes are being added for message controller

--- src/routes/message.route.ts
+++ src/routes/message.route.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
-import { sendMessage } from "../controllers/message.controller";
+import {
+  sendMessage,
+  getProjectMessages,
+  markMessagesAsRead,
+  getMessageById,
+  deleteMessage,
+  //getMessageStats,
+  searchMessages,
+} from "../controllers/message.controller";
 import { authenticate } from "../middlewares/auth.middleware";
 import { validateMessage } from "../middlewares/validation.middleware";
 const router = Router({ mergeParams: true });
@@ -7,5 +15,21 @@ const router = Router({ mergeParams: true });
 router
   .route("/projects/:projectId/messages")
   .post(authenticate, validateMessage, sendMessage);
+
+router
+  .route("/projects/:projectId/messages")
+  .get(authenticate, getProjectMessages);
+
+router
+  .route("/projects/:projectId/messages/read")
+  .patch(authenticate, markMessagesAsRead);
+
+router.route("/messages/:messageId").get(authenticate, getMessageById);
+
+router.route("/messages/:messageId").delete(authenticate, deleteMessage);
+
+router
+  .route("/projects/:projectId/messages/search")
+  .get(authenticate, searchMessages);
 
 export default router;
