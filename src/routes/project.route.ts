e7778e8f2b33d0be21eb67277bbc245a2f6f57dd
BASHIR AHMED MONDAL <bashirmondal03@gmail.com>
2025-11-27 22:50:41 +0530

    fix: removed upload function in create project route

--- src/routes/project.route.ts
+++ src/routes/project.route.ts
@@ -6,10 +6,10 @@ import {
   editProject,
 } from "../controllers/project.controller";
 import { authenticate } from "../middlewares/auth.middleware";
-import { upload } from "../middlewares/multer.middleware";
+
 const router = Router({ mergeParams: true });
 
-router.route("/create-project").post(authenticate, upload, createProject);
+router.route("/create-project").post(authenticate,  createProject);
 router.route("/get-project/:projectDisplayId").get(authenticate, getProject);
 router.route("/get-project-id").get(authenticate, getProjectIds)
 router.route("/edit-project").post(authenticate, editProject);
